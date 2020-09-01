1ad6464af5b895f091de131dfa9cea6f1419b7a5
mjyyy1 <malte.erlandsson@elev.ga.ntig.se>
2020-09-01 10:51:28 +0200

    yikes

--- FirstServer/app.js
+++ FirstServer/app.js
@@ -2,5 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const ClientDir =__dirname + "\\Client"
+
+app.get('/', (req, res) => res.sendfile(ClientDir + "\\index.html"))
+app.get('/teknik', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
