1a322421f3b9508b02f1d6c92f12c1bd4072841f
mjyyy1 <malte.erlandsson@elev.ga.ntig.se>
2020-09-03 13:27:51 +0200

    k

--- FirstServer/app.js
+++ FirstServer/app.js
@@ -2,8 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const ClientDir =__dirname + "\\Client"
+const ClientDir =__dirname + "\\Client\\"
 
-app.get('/', (req, res) => res.sendfile(ClientDir + "\\index.html"))
+app.get('/', (req, res) => res.sendfile(ClientDir + "index.html"))
 app.get('/teknik', (req, res) => res.send('Hello World!'))
+
+app.get('/', (req, res) => {
+  res.send(ClientDir + "style.css")
+})
+app.get('/', (req, res) => {
+  res.send('zombie.png')
+})
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
