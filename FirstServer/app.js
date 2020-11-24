5556c6c93a63e7978b3e6891d7e188771c53199a
mjyyy1 <54800274+mjyyy1@users.noreply.github.com>
2020-11-24 10:24:14 +0100

    m

--- FirstServer/app.js
+++ FirstServer/app.js
@@ -10,7 +10,10 @@ app.set('view engine', 'ejs')
 
 app.use(express.json())
 app.use(express.urlencoded())
+app.use(express.static())
 
+
+app.set('view engine', 'ejs')
 
 app.get('/',(req,res) => {
   res.render('pages/index.ejs',{name:""})
